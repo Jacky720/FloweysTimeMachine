66812f38fdca939c15b5effa54188710bcfad465
Jacky720 <32578221+Jacky720@users.noreply.github.com>
2021-11-30 10:04:12 -0500

    Add Dirty Hacker preset w/ corrupt Papyrus flag

---
+++ presets/dirty-hacker.js
@@ -0,0 +1,7 @@
+presets["Dirty Hacker"] = JSON.parse(JSON.stringify(presets["Ruins Start"])); // why is deep copying so hard
+
+presets["Dirty Hacker"].ini.General.Name = "Hacker";
+presets["Dirty Hacker"].ini.General.Room = "238";
+presets["Dirty Hacker"].lines[0] = "Hacker";
+presets["Dirty Hacker"].lines[97] = "2";
+presets["Dirty Hacker"].lines[547] = "238";
